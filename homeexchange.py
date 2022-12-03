67e5be1de3c385cf91abcced2e133e6b0312a6a9
justderrickgyimah <114161844+justderrickgyimah@users.noreply.github.com>
2022-12-03 10:17:03 +0000

    Update homeexchange.py

--- homeexchange.py
+++ homeexchange.py
@@ -2,10 +2,18 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
-from openpyxl import load_workbook, Workbook
+from openpyxl import Workbook
+
+properties = []
+
+wb = Workbook()
+ws = wb.active
+ws.title = "Extracted Data"
+ws.append(["Title", "Rating","Capacity", "Location", "Price", "Number of Reviews"])
 
 
-for x in range(1, 2):
+
+for x in range(1,30):
     url = ("https://www.homeexchange.com/search-v2/everywhere?bounds=-85%2C-180%2C85%2C180&place_id=false")
     driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.get(url + "&page="+str(x))
@@ -13,19 +21,38 @@ for x in range(1, 2):
     soup = BeautifulSoup(driver.page_source, "html.parser")
     lists = soup.findAll(class_="search-result col-sm-6")
 
-
-
     for lists in lists:
         title = lists.find(class_="title").text
-        # rating = lists.find(class_="rating").text
-        # capacity = lists.find(class_="capacity").text
+        try:
+            rating = lists.find(class_="rating").text
+        except AttributeError:
+            rating = "Not Available"
+        try:
+            capacity = lists.find(class_="capacity").text
+        except AttributeError:
+            capacity = "Not Available"
+
         location = lists.find(class_="home-location").text
         price = lists.find(class_="homebox-review-gp").text
-        # number_of_exchanges = lists.find(class_="icon-home-exchange").text
 
-        homebox = [title, #rating, capacity,
-                   location, price, #number_of_exchanges
-                   ]
+        try:
+            number_of_reviews = lists.find(itemprop="reviewCount").text
+        except AttributeError:
+            number_of_reviews = "Not Available"
 
-        print(homebox)
 
+
+        homebox = [title, rating,  capacity,
+                    location, price,  number_of_reviews
+                    ]
+        properties.append(homebox)
+        ws.append(homebox)
+
+
+df = pd.DataFrame(properties)
+df.columns = ["Title", "Rating","Capacity", "Location", "Price", "Number of Reviews"]
+
+
+
+
+wb.save("Home Exchange.xlsx")
